e0c4a2aed568ce4733e621205bc81835a9abe797
albertjson <1300539429@qq.com>
2016-10-09 00:07:22 +0800

    commit code
    no

--- SZCEvolution/Classes/Classes/Frameworks/Network/ZCBaseRequest.m
+++ SZCEvolution/Classes/Classes/Frameworks/Network/ZCBaseRequest.m
@@ -16,5 +16,15 @@
 {
     NSLog(@"我失败了111想");
 }
+/**
+ 1. 若需要定义json--model，可在以下方法中处理  class ： YTKNetworkAgent
+ - (void)handleRequestResult:(NSURLSessionTask *)task responseObject:(id)responseObject
+ 2. 需要进一步处理error，例如error message定义等等，也在上面方法进行处理
+ 3. 需要把服务端正常返回的错误格式的数据定义为http的错误，待处理
+ 4. 需要把token失效的问题统一处理的错误，待处理
+ @param success <#success description#>
+ @param failure <#failure description#>
+ */
+
 
 @end
